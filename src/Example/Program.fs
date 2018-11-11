2cac3dcab11a50b8af03ea61034665586720c320
Dani J <dani.user@gmail.com>
2018-11-11 16:39:10 +0200

    Added initial example

--- src/Example/Program.fs
+++ src/Example/Program.fs
@@ -1,8 +1,18 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+open Animations
+open Animations.Output
+
+let createSampleAnimation(): unit = 
+
+    using (new RawFrameOutput("frame-{0:0000}.raw")) (fun output ->
+
+        let animation = new Animation(800, 600, FrameCount(10), 2.0, output)
+        animation.Run()
+    )
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+
+    createSampleAnimation()
+    0
---
+++ src/Framework/Animation.fs
@@ -0,0 +1,21 @@
+﻿namespace Animations
+
+open Animations.Output
+open System
+
+type public AnimationLength = 
+    | FrameCount of int
+    | Seconds of float
+
+type public Animation(width: int, height: int, length: AnimationLength, fps: float, output: IVideoOutput) =
+
+    let frame = new Frame(width, height)
+    let frameCount = 
+        match length with
+            | FrameCount count -> count
+            | Seconds seconds -> int (Math.Ceiling(seconds * fps))
+
+    member public this.Run(): unit = 
+
+        for i = 0 to frameCount - 1 do
+            output.Write(frame)
