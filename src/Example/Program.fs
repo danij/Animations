fbf32d024b505ee4bd5eb92f1056ce69012838a7
Dani J <dani.user@gmail.com>
2018-11-11 17:01:01 +0200

    Implemented background color

--- src/Example/Program.fs
+++ src/Example/Program.fs
@@ -1,13 +1,12 @@
-﻿open System
-
-open Animations
+﻿open Animations
 open Animations.Output
+open SkiaSharp
 
 let createSampleAnimation(): unit = 
 
     using (new RawFrameOutput("frame-{0:0000}.raw")) (fun output ->
 
-        let animation = new Animation(800, 600, FrameCount(10), 2.0, output)
+        let animation = new Animation(800, 600, FrameCount(10), 2.0, SKColors.AliceBlue, output)
         animation.Run()
     )
 
--- src/Framework/Animation.fs
+++ src/Framework/Animation.fs
@@ -1,21 +1,41 @@
 ﻿namespace Animations
 
 open Animations.Output
+open SkiaSharp
 open System
+open System.Runtime.InteropServices
 
 type public AnimationLength = 
     | FrameCount of int
     | Seconds of float
 
-type public Animation(width: int, height: int, length: AnimationLength, fps: float, output: IVideoOutput) =
+type public Animation(width: int, height: int, length: AnimationLength, fps: float, 
+                      backgroundColor: SKColor, output: IVideoOutput) =
 
     let frame = new Frame(width, height)
     let frameCount = 
         match length with
             | FrameCount count -> count
             | Seconds seconds -> int (Math.Ceiling(seconds * fps))
+    let imageInfo = new SKImageInfo(width, height, SKColorType.Bgra8888)
+
+    let DrawOnCurrentFrame(callback: SKCanvas -> unit): unit =
+
+        let pinnedBytes = GCHandle.Alloc(frame.Bytes, GCHandleType.Pinned)
+        using (SKSurface.Create(imageInfo, pinnedBytes.AddrOfPinnedObject(), frame.BytesPerRow)) (fun surface ->
+        
+            let canvas = surface.Canvas
+            canvas.Clear(backgroundColor)
+            callback(canvas)
+        )
+        pinnedBytes.Free()
+
+    let RenderFrame(index: int): unit = 
+
+        DrawOnCurrentFrame(fun canvas -> ())
 
     member public this.Run(): unit = 
 
         for i = 0 to frameCount - 1 do
+            RenderFrame(i)
             output.Write(frame)
